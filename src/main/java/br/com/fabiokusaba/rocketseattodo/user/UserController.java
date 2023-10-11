cfc070976e88d3209b7a44a97cd92648bfccb7c7
Fabio Kusaba <fabinhokb44@gmail.com>
2023-10-11 20:02:12 -0300

    Aula 03

--- src/main/java/br/com/fabiokusaba/rocketseattodo/user/UserController.java
+++ src/main/java/br/com/fabiokusaba/rocketseattodo/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.fabiokusaba.rocketseattodo.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,10 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
 
         var userCreated = userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
